5f891a82990081fd7ba529953303f72ceb400f53
zdziwionyrysiczeq <54484477+mrchomik892@users.noreply.github.com>
2020-09-29 18:17:12 +0200

    Add files via upload

---
+++ auth.php
@@ -0,0 +1,11 @@
+<?php
+if($_POST['1fa'] == user)
+if($_POST['2fa'] == user)
+{
+include "site.html";
+} else {
+include "index.html";
+?>
+<h1>True 2FA</h1>
+<h2>Last edit 6:11 PM 9/29/2020</h2>
+<h3>Build 1</h3>
